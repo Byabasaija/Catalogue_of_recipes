44c4c55b2a337434bfc9b44eb5b725c628fa4068
Byabasaija <basaijapascal9@gmail.com>
2022-02-20 18:02:25 +0300

    fix header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 const Header = ({ categories }) => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container">
       <Link className="navbar-brand text-white pg-title" to="/">Catalogue of Recipes</Link>
       <button className="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon text-white" />
+        <span className=" navbar-toggler-icon" />
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -22,7 +22,7 @@ const Header = ({ categories }) => (
               aria-expanded="false"
             >
               Change categories
-              <span role="img" aria-label="backhand">👉</span>
+              <span role="img" aria-label="backhand"> 👉</span>
             </span>
             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
               {categories.map((category) => (
